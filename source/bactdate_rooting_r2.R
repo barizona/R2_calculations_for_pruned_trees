e3158a3ed5fd9ae5ea894acf8c67eb27e3153028
barizona <barizona@barizona>
2024-11-05 16:06:18 +0100

    change in bactdate rooting script

--- source/bactdate_rooting_r2.R
+++ source/bactdate_rooting_r2.R
@@ -1,6 +1,8 @@
 library(tidyverse)
+source("source/01_creating_lists_of_trees.R")
 source("source/functions.R")
 
+# run on server !!!!!!!!!!
 
 #xxxxxxxxxxxxxxxxxxxxxxxxxx
 # Bactdate rooting, R2 ----------------------------------------------------
@@ -8,28 +10,35 @@ source("source/functions.R")
 
 run_bactdate_parallel(tree_list = tree_list_unpruned_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/unpruned/")
+                      output_fig_dir = "output/unpruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_treeshrink_v1_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/treeshrink_v1_pruned/")
+                      output_fig_dir = "output/treeshrink_v1_pruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_iqr_rooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/iqr_pruned/")
+                      output_fig_dir = "output/iqr_pruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_PSFA_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/PSFA_pruned/")
+                      output_fig_dir = "output/PSFA_pruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_CPA_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/CPA_pruned/")
+                      output_fig_dir = "output/CPA_pruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_CPA_PSFA_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/CPA+PSFA_pruned/")
+                      output_fig_dir = "output/CPA+PSFA_pruned/",
+                      num_cores = 25)
 
 run_bactdate_parallel(tree_list = tree_list_PSFA_CPA_PSFA_unrooted,
                       date_tab = date_tab_full,
-                      output_fig_dir = "output/PSFA+CPA+PSFA_pruned/")
+                      output_fig_dir = "output/PSFA+CPA+PSFA_pruned/",
+                      num_cores = 25)
